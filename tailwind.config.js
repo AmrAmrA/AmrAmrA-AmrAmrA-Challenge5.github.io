179524e2f6b34a28370ed138fdfaab316fe64207
Abd El Fattah Amr <amrabdelfattah@hotmail.fr>
2022-04-04 15:02:21 +0200

    Responsive mobile first and adding background SVG

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'flexWrap' : '865px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }, 
     extend: {},
   },
   plugins: [],
